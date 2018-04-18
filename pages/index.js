caa9bfee9ce786291e0e834903f535f93f971040
線上讀書會 <polo13999@gmail.com>
2018-04-18 14:50:14 +0800

    feat(add prettier feat): prettier feat

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,6 @@ import React from 'react'
 
 import Layout from '../src/components/layout'
 import { MiddleBody } from '../src/components/middleBody'
-const Index = () => (
-  <div>
-    <MiddleBody />
-  </div>
-)
+const Index = () => <div />
 
 export default Layout(Index)
