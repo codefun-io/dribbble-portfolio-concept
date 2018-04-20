8072d5d1a0bf7e156536aa087d58891a6b0b03ab
線上讀書會 <polo13999@gmail.com>
2018-04-20 16:29:01 +0800

    fix: rename textArea to section

--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,16 @@ import React from 'react'
 
 import Layout from '../src/components/layout'
 import Section1 from '../src/components/section1'
+import Section2 from '../src/components/section2'
 import GetUpdate from '../src/components/get-update'
 import Cat from '../src/components/cat'
 import { Hero } from '../src/components/hero'
 import Gallery from '../src/components/gallery'
-import TextArea from '../src/components/textArea'
 
 const Index = () => (
   <div className="main-content">
     <Section1 />
-    <TextArea />
-    <TextArea2 />
+    <Section2 />
 
     <Gallery />
     <Hero />
