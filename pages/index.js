1d2fb0e28f8d35f5c44181213746464d352b182a
線上讀書會 <polo13999@gmail.com>
2018-04-20 20:17:19 +0800

    feat: add section3 and rename textArea

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import React from 'react'
 import Layout from '../src/components/layout'
 import Section1 from '../src/components/section1'
 import Section2 from '../src/components/section2'
+import Section3 from '../src/components/section3'
 import GetUpdate from '../src/components/get-update'
 import Cat from '../src/components/cat'
 import { Hero } from '../src/components/hero'
@@ -12,7 +13,7 @@ const Index = () => (
   <div className="main-content">
     <Section1 />
     <Section2 />
-
+    <Section3 />
     <Gallery />
     <Hero />
     <Cat />
--- src/components/section3.js
+++ src/components/section3.js
@@ -3,52 +3,37 @@ import styled from 'styled-components'
 
 const Wrap = styled.div`
   display: grid;
-  grid-template-columns: 1fr 1.5fr 1fr;
+  grid-template-columns: 1fr 3.5fr 1fr;
   grid-template-rows: 40px 60px 1fr;
   min-height: 300px;
   justify-content: center;
   padding-top: 50px;
 `
 
-const Titleh4 = styled.div`
-  font-size: 0.5em;
-  letter-spacing: 5px;
+const WrapInner = styled.div`
   grid-column: 2/3;
   grid-row: 1;
-  display: flex;
-  justify-content: center;
-  flex: 1;
-  color: #bfbfbf;
-  font-weight: 900;
+  display: grid;
+  grid-template-columns: 1fr 30px 3fr 3fr;
+  grid-template-rows: auto;
 `
-const WrapTitle2 = styled.div`
+
+const Num = styled.div`
+  color: #ff377c;
+  font-size: 3em;
+  margin-top: 20px;
+
   grid-column: 2/3;
-  font-size: 4em;
-  grid-row: 2;
-  letter-spacing: 4px;
-  display: flex;
-  justify-content: center;
-  white-space: nowrap;
-  flex: 1;
-  @media (max-width: 1000px) {
-    font-size: 3em;
-  }
-  @media (max-width: 850px) {
-    font-size: 2.5em;
-  }
-  @media (max-width: 720px) {
-    font-size: 1.5em;
-  }
+  grid-row: 3;
 `
 
 const Titleh3 = styled.div`
-  grid-column: 2/3;
+  grid-column: ${props => props.data.column};
   font-size: 0.9em;
   grid-row: 3;
-
   justify-content: center;
   flex: 1;
-  text-align: center;
+  padding-left: 20px;
   color: #8e8e8e;
 
   @media (max-width: 1000px) {
@@ -64,12 +49,19 @@ const Titleh3 = styled.div`
 
 export default () => (
   <Wrap>
-    <Titleh4>DESIGN STUDIO</Titleh4>
-    <WrapTitle2>See Us Live</WrapTitle2>
-    <Titleh3>
-      But it's just using an old paradigm to describe something new, and when
-      you do that, you're tying the idea to all the weight and baggage that
-      accompanied the old. You're taking the lazy option
-    </Titleh3>
+    <WrapInner>
+      <Num>02</Num>
+
+      <Titleh3 data={{ column: '3/4' }}>
+        But it's just using an old paradigm to describe something new, and when
+        you do that, you're tying the idea to all the weight and baggage that
+        accompanied the old. You're taking the lazy option
+      </Titleh3>
+      <Titleh3 data={{ column: '4/4' }}>
+        But it's just using an old paradigm to describe something new, and when
+        you do that, you're tying the idea to all the weight and baggage that
+        accompanied the old. You're taking the lazy option
+      </Titleh3>
+    </WrapInner>
   </Wrap>
 )
